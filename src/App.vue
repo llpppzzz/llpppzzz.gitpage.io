a4d9a8687319b204da3ba4d96c75e7a399df6452
lipeizhou <llpppzzz@163.com>
2017-11-23 16:56:29 +0800

    引入elementui

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
 	<div id="app">
-		<!--<img src="./assets/logo.png">-->
-		<!--<h1>{{ msg }}</h1>-->
-		<!--<h2>Essential Links</h2>-->
-		<!--<ul>-->
-		<!--<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>-->
-		<!--<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>-->
-		<!--<li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>-->
-		<!--<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>-->
-		<!--</ul>-->
-		<!--<h2>Ecosystem</h2>-->
-		<!--<ul>-->
-		<!--<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>-->
-		<!--<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>-->
-		<!--<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>-->
-		<!--<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>-->
-		<!--</ul>-->
-		<ul>
-			<li v-for="(item,index) in list" @click="clickHandler(item.id,index)">
-				<span :class="{'red': index === currentIndex}">{{item.num}}</span>
-			</li>
-		</ul>
-		<div class="bottom-button">
-			<button @click="showDiv()">操作1</button>
-			<button>操作2</button>
-			<button>操作3</button>
+		<div class="carousel-box">
+			<el-carousel indicator-position="outside" height="600px">
+				<el-carousel-item v-for="(item, index) in imgs" :key="index">
+					<img :src="item.src" alt="">
+				</el-carousel-item>
+			</el-carousel>
 		</div>
 	</div>
 </template>
@@ -34,44 +15,17 @@
 		name: 'app',
 		data() {
 			return {
-				msg: 'Welcome to Your Vue.js App',
-				list: [
-					{
-						num: 40,
-						id: 1
-					},
-					{
-						num: 40,
-						id: 2
-					},
-					{
-						num: 40,
-						id: 3
-					},
-					{
-						num: 40,
-						id: 4
-					},
-					{
-						num: 40,
-						id: 5
-					},
-					{
-						num: 40,
-						id: 6
-					},
-				],
-				currentIndex: '',
-				currentId: '',
+				imgs: [
+					{src: '../static/images/dfd.jpg'},
+//					{src: '../static/images/dfd.jpg'},
+//					{src: '../static/images/dfd.jpg'},
+//					{src: '../static/images/dfd.jpg'}
+				]
 			}
 		},
 		methods: {
-			clickHandler: function (id, index) {
-				this.currentId = id;
-				this.currentIndex = index;
-			},
-			showDiv: function () {
-				console.log('id:'+this.currentId, 'index:'+this.currentIndex);
+			clickImg: function () {
+				console.log('ssss');
 			}
 		}
 	}
@@ -84,7 +38,7 @@
 		-moz-osx-font-smoothing: grayscale;
 		text-align: center;
 		color: #2c3e50;
-		margin-top: 60px;
+		/*margin-top: 60px;*/
 	}
 
 	h1, h2 {
@@ -108,4 +62,10 @@
 	.red {
 		color: red;
 	}
+
+	.el-carousel__item img {
+		width: 100%;
+	}
+
+
 </style>
