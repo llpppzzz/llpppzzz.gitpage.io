aaa9176fc426c2a16686213846987a7efc057e42
lipeizhou <llpppzzz@163.com>
2020-12-04 15:30:50 +0800

    install element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+
+installElementPlus(App)
 
 createApp(App).use(store).use(router).mount('#app')
